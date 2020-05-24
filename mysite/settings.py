39627026d813b3633847a6f230ecbcb76627d1bf
Ishita Tiwari <ishitatiwari23@gmail.com>
2020-05-24 19:05:34 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r!7_(5+4)yk@%a6tv(o564invo=p0-4k&mf3x&#pl88bxa6o)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 TINYMCE_DEFAULT_CONFIG = {
     'height': 360,
